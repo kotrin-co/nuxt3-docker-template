61d56c585565dd8a99a2efb0c96af5cf4138e6bd
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2022-07-07 17:14:11 +0900

    Initial commit

---
+++ components/SimpleCounter.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { useCounterStore } from '~/stores/counter';
+
+const counter = useCounterStore();
+</script>
+
+<template>
+  <div id="counter-container">
+    <p id="value">
+      Counter is currently set to {{ counter.value }}
+      <strong id="perfect-square" v-if="counter.isPerfectSquare">
+        <em>🎉 IT'S A PERFECT SQUARE 🎉</em>
+      </strong>
+    </p>
+    <div id="actions">
+      <button @click="counter.increment">Increment</button>
+      <button @click="counter.decrement">Decrement</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#counter-container {
+  line-height: 1.5rem;
+  text-align: center;
+}
+
+#value {
+  font-weight: bold;
+}
+
+#actions {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+}
+</style>
